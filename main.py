396dc79b516f9fc95e5828cd7408207081713a06
gilberet <gilberet@hotmail.com>
2022-06-07 05:15:49 -0400

    Primer commit

---
+++ main.py
@@ -0,0 +1,4 @@
+
+IMG_COURCE_XPATH = '//div[@class="LearningPathDetail"]//div[@class="LearningPathDetail-step LearningPathDetail-step--school"]//a[@class="Course Course--removable"]//img/@src'
+TITLE_COURCE_XPATH = '//div[@class="LearningPathDetail"]//div[@class="LearningPathDetail-step LearningPathDetail-step--school"]//a[@class="Course Course--removable"]//img/@alt'
+LINK_COURCE_XPATH = '//div[@class="LearningPathDetail"]//div[@class="LearningPathDetail-step LearningPathDetail-step--school"]//a[@class="Course Course--removable"]/@href'
